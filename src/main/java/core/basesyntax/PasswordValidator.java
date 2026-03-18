97d88c5c0fd07db52fdb76fda0c08718c56fb9c5
Maksym <pozetiv13@gmail.com>
2026-03-18 16:46:41 +0200

    Maksym Gorodnychyi

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,11 +1,12 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (!password.equals(repeatPassword)){
+    public void validate(String password,
+                         String repeatPassword) throws PasswordValidationException {
+        if (!password.equals(repeatPassword)) {
             throw new PasswordValidationException ("Wrong passwords");
         }
-        if (password.length()<10 || repeatPassword.length()<10){
+        if (password.length()<10 || repeatPassword.length()<10) {
             throw new PasswordValidationException ("Wrong passwords");
         }
     }
